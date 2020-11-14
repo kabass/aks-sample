90a7780e874b0eea4d322570ef8be01472c25057
Arthur Tchamokouen <rodrigue.arthur@gmail.com>
2020-11-14 09:13:44 +0100

    adding_commit_command

--- commit.cmd
+++ commit.cmd
@@ -1,5 +1,5 @@
+if "%1"=="" goto :error
 
-if "%1"=="" goto :done
 git status
 git add .
 git commit -m %1
@@ -7,6 +7,10 @@ git pull
 git status
 git commit -m "merge"
 git push
+goto :done
+
+:error
+echo No parameter. Please enter the commit message and retry.
 
 :done
-echo No parameter. Please enter the commit message and retry
+echo done
--- commit.sh
+++ commit.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-if [[ $1 ]] then;
-  echo "please specify the commit message and retry"
-git status
-git add .
-git commit -m $1
-git pull
-git status
-git commit -m "merge"
-git push
+if [ "$#" -eq 0 ] ; then 
+    echo "No parameter. Please enter the commit message and retry."
+else
+    git status
+    git add .
+    git commit -m $1
+    git pull
+    git status
+    git commit -m "merge"
+    git push
+    
+    echo "done"
+fi
