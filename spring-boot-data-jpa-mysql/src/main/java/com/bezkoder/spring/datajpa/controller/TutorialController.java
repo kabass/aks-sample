86f27227098b7a32e984abce3eff7ac961d13850
bka <ka.bassirou@gmail.com>
2020-11-16 09:49:08 +0100

    password change

--- spring-boot-data-jpa-mysql/src/main/java/com/bezkoder/spring/datajpa/controller/TutorialController.java
+++ spring-boot-data-jpa-mysql/src/main/java/com/bezkoder/spring/datajpa/controller/TutorialController.java
@@ -36,7 +36,7 @@ public class TutorialController {
 	public ResponseEntity<List<Tutorial>> getAllTutorials(@RequestParam(required = false) String title) {
 		try {
 
-			LOGGER.info("getting all tutorials update 4");
+			LOGGER.info("getting all tutorials update 5");
 			List<Tutorial> tutorials = new ArrayList<Tutorial>();
 
 			if (title == null)
