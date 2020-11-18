bea5459de33610bdc9053ee92a9eaed2a0e5d845
Arthur Tchamokouen <rodrigue.arthur@gmail.com>
2020-11-18 09:31:38 +0100

    merge

--- terraform/aks/main.tf
+++ terraform/aks/main.tf
@@ -28,46 +28,6 @@ resource "azurerm_log_analytics_solution" "test" {
   }
 }
 
-data "azurerm_key_vault" "keyvault" {
-  name                = var.keyvault_name
-  resource_group_name = var.kv_resource_group_name
-}
-
-resource "azurerm_user_assigned_identity" "identity" {
-  resource_group_name = azurerm_resource_group.k8s_rg.name
-  location            = var.log_analytics_workspace_location
-
-  name = var.pod_identity
-}
-
-data "azuread_client_config" "current" {
-}
-
-resource "azurerm_role_assignment" "reader_kv" {
-  scope                = data.azurerm_key_vault.keyvault.id
-  role_definition_name = "Reader"
-  principal_id         = azurerm_user_assigned_identity.identity.principal_id
-}
-
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_key_vault_access_policy" "identity_get" {
-  key_vault_id = data.azurerm_key_vault.keyvault.id
-
-  tenant_id = data.azurerm_client_config.current.tenant_id
-  object_id = data.azurerm_client_config.current.object_id
-  //tenant_id = data.azuread_client_config.current.tenant_id
-  //object_id = data.azuread_client_config.current.object_id
-
-  key_permissions = [
-    "get",
-  ]
-
-  secret_permissions = [
-    "get",
-  ]
-}
-
 resource "azurerm_kubernetes_cluster" "k8s_cluster" {
   name                = var.cluster_name
   location            = azurerm_resource_group.k8s_rg.location
@@ -92,6 +52,10 @@ resource "azurerm_kubernetes_cluster" "k8s_cluster" {
   }
 
   addon_profile {
+    kube_dashboard {
+      enabled = true
+    }
+
     oms_agent {
       enabled                    = true
       log_analytics_workspace_id = azurerm_log_analytics_workspace.test.id
@@ -109,30 +73,85 @@ resource "azurerm_kubernetes_cluster" "k8s_cluster" {
 }
 
 resource "null_resource" "csi-secrets-store-provider-azure_aad-pod-identity" {
+  depends_on = [azurerm_kubernetes_cluster.k8s_cluster]
+
   provisioner "local-exec" {
-    command = "helm repo add csi-secrets-store-provider-azure https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts; helm install csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --generate-name"
+    command = "az aks get-credentials --name ${azurerm_kubernetes_cluster.k8s_cluster.name} --overwrite-existing --resource-group ${azurerm_kubernetes_cluster.k8s_cluster.resource_group_name}"
   }
 
   provisioner "local-exec" {
-    command = "helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts;helm install pod-identity aad-pod-identity/aad-pod-identity"
+    command = "helm repo add csi-secrets-store-provider-azure https://raw.githubusercontent.com/Azure/secrets-store-csi-driver-provider-azure/master/charts"
+  }
+  provisioner "local-exec" {
+    command = "helm install csi-secrets-store-provider-azure/csi-secrets-store-provider-azure --generate-name"
+  }
+
+  provisioner "local-exec" {
+    command = "helm repo add aad-pod-identity https://raw.githubusercontent.com/Azure/aad-pod-identity/master/charts"
+  }
+  provisioner "local-exec" {
+    command = "helm install pod-identity aad-pod-identity/aad-pod-identity"
   }
 }
-/*
+
+data "azurerm_key_vault" "keyvault" {
+  name                = var.keyvault_name
+  resource_group_name = var.kv_resource_group_name
+}
+
+resource "azurerm_user_assigned_identity" "identity" {
+  resource_group_name = azurerm_resource_group.k8s_rg.name
+  location            = var.log_analytics_workspace_location
+
+  name = var.pod_identity
+}
+
+data "azuread_client_config" "current" {}
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_role_assignment" "reader_kv" {
+  scope                = data.azurerm_key_vault.keyvault.id
+  role_definition_name = "Reader"
+  principal_id         = azurerm_user_assigned_identity.identity.principal_id
+}
+
+resource "azurerm_key_vault_access_policy" "identity_get" {
+  key_vault_id = data.azurerm_key_vault.keyvault.id
+
+  tenant_id      = data.azurerm_client_config.current.tenant_id
+  object_id      = azurerm_user_assigned_identity.identity.client_id
+  application_id = azurerm_user_assigned_identity.identity.client_id
+
+  key_permissions = [
+    "get",
+  ]
+
+  secret_permissions = [
+    "get",
+  ]
+}
+
 resource "azurerm_role_assignment" "Managed_Identity_Operator_1" {
   scope                = azurerm_resource_group.k8s_rg.id
   role_definition_name = "Managed Identity Operator"
-  principal_id         = azurerm_kubernetes_cluster.k8s_cluster.principal_id
+  principal_id         = azurerm_kubernetes_cluster.k8s_cluster.identity[0].principal_id
+}
+
+data "azurerm_resource_group" "k8s_rg_node" {
+  name     = azurerm_kubernetes_cluster.k8s_cluster.node_resource_group  
 }
 
 resource "azurerm_role_assignment" "Managed_Identity_Operator_2" {
-  scope                = azurerm_kubernetes_cluster.k8s_cluster.node_resource_group
+  scope                = data.azurerm_resource_group.k8s_rg_node.id
+  
   role_definition_name = "Managed Identity Operator"
-  principal_id         = azurerm_kubernetes_cluster.k8s_cluster.principal_id
+  principal_id         = azurerm_kubernetes_cluster.k8s_cluster.identity[0].principal_id
 }
 
 resource "azurerm_role_assignment" "Virtual_Machine_Contributor" {
-  scope                = azurerm_kubernetes_cluster.k8s_cluster.node_resource_group
+  scope                = data.azurerm_resource_group.k8s_rg_node.id
+
   role_definition_name = "Virtual Machine Contributor"
-  principal_id         = azurerm_kubernetes_cluster.k8s_cluster.principal_id
+  principal_id         = azurerm_kubernetes_cluster.k8s_cluster.identity[0].principal_id
 }
-*/
